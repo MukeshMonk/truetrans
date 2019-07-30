e0d1a665465399db1b412f6b948be37bcd5eaf1d
MukeshMonk <mukesh@monkdesk.in>
2019-07-30 19:02:59 +0530

    all update

--- src/App.js
+++ src/App.js
@@ -11,15 +11,12 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-          <Router basename={'/'}>
+          <Router>
               <div>
                   <div className="content">
-                    <Route exact path={`${process.env.PUBLIC_URL}/`} component={Login} />
-                    <Route path={`${process.env.PUBLIC_URL}/dashboard`} component={Dashboard} />
-                    <Route path={`${process.env.PUBLIC_URL}/register`} component={Registration} />
-                    {/* <Route  exact path="/" component={Login}/>
+                    <Route  exact path="/" component={Login}/>
                     <Route  path="/dashboard" component={Dashboard} />
-                    <Route path="/register" component={Registration} />  */}
+                    <Route path="/register" component={Registration} /> 
                   </div>
               </div>
           </Router>
