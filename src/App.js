8416be5444f164ed4b211fabc31c43116e216c55
MukeshMonk <mukesh@monkdesk.in>
2019-07-31 12:05:36 +0530

    31-jul-2019 12.05

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import './App.css';
 import Login from './componets/Login/Login';
 import Registration from './componets/Registration/Registration';
 import  Dashboard from './componets/Dashboard/Dashboard';
-
+import { createBrowserHistory } from "history";
 
 class App extends Component {
   render() {
+    const history = createBrowserHistory();
     return (
       <div className="app">
-          <Router>
+          <Router history={history}>
               <div>
                   <div className="content">
-                    <Route  startsWith path="/" component={Login}/>
+                    <Switch>
+                    <Route  exact path="/" component={Login}/>
                     <Route  path="/dashboard" component={Dashboard} />
                     <Route path="/register" component={Registration} /> 
+                    </Switch>
                   </div>
               </div>
           </Router>
