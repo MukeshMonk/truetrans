82eced8fdcfb735fe298b93cb89a9c5eec53002d
MukeshMonk <mukesh@monkdesk.in>
2019-07-31 12:25:59 +0530

    31-jul-2019 12.22

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Route,Router} from 'react-router-dom';
+import { Route,BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 /* Components */
 import Login from './componets/Login/Login';
 import Registration from './componets/Registration/Registration';
 import  Dashboard from './componets/Dashboard/Dashboard';
+
 
 class App extends Component {
   render() {
@@ -13,11 +14,9 @@ class App extends Component {
           <Router>
               <div>
                   <div className="content">
-                   
                     <Route  exact path="/" component={Login}/>
                     <Route  path="/dashboard" component={Dashboard} />
                     <Route path="/register" component={Registration} /> 
-                    
                   </div>
               </div>
           </Router>
